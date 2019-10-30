54129c9cfd543cbfc22be7e69b9ce586ea3dd43f
joshuakery <36903306+joshuakery@users.noreply.github.com>
2019-10-30 13:49:08 -0400

    Updates

---
+++ precache-manifest.c587deef8f09dab6e904b37bcb7bd5af.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfe333234d78b111f5ada132fa38c65",
+    "url": "/mytour/index.html"
+  },
+  {
+    "revision": "a81d689962871300f553",
+    "url": "/mytour/static/css/main.541506c5.chunk.css"
+  },
+  {
+    "revision": "826732f8713e97de8628",
+    "url": "/mytour/static/js/2.125304d6.chunk.js"
+  },
+  {
+    "revision": "a81d689962871300f553",
+    "url": "/mytour/static/js/main.e962eedc.chunk.js"
+  },
+  {
+    "revision": "d41960e6fc91ee3956d5",
+    "url": "/mytour/static/js/runtime-main.53878d57.js"
+  },
+  {
+    "revision": "b9e6809ec4428775a21d7a98ecd87476",
+    "url": "/mytour/static/media/btn_down.b9e6809e.png"
+  },
+  {
+    "revision": "8e2ab890fa4a30b42116f184f16fdeee",
+    "url": "/mytour/static/media/btn_left.8e2ab890.png"
+  },
+  {
+    "revision": "45eabe5d08722736468ee3f56082a1ed",
+    "url": "/mytour/static/media/btn_right.45eabe5d.png"
+  },
+  {
+    "revision": "abf022a28502598c5817a21dc4b1da88",
+    "url": "/mytour/static/media/btn_up.abf022a2.png"
+  },
+  {
+    "revision": "1a3f101d3c1348bccf25c3de0df8b181",
+    "url": "/mytour/static/media/logo.1a3f101d.png"
+  },
+  {
+    "revision": "deb7d49bcb2aa81997aaaa523636d9ab",
+    "url": "/mytour/static/media/room-basement.deb7d49b.jpg"
+  },
+  {
+    "revision": "21ac5b18e7e91e06b4dd31d23186daf0",
+    "url": "/mytour/static/media/room-dining-room.21ac5b18.jpg"
+  },
+  {
+    "revision": "bc9a50012e805ed435e50f5b77325ce4",
+    "url": "/mytour/static/media/room-foyer.bc9a5001.jpg"
+  },
+  {
+    "revision": "d946d188fec3d907674e77c22908210a",
+    "url": "/mytour/static/media/room-hallway.d946d188.jpg"
+  },
+  {
+    "revision": "16ce996d29c62632dc80f86527e46d15",
+    "url": "/mytour/static/media/room-living-room.16ce996d.jpg"
+  },
+  {
+    "revision": "2f6b30927b028424c1d61a402ead9ade",
+    "url": "/mytour/static/media/room-long-sunroom.2f6b3092.jpg"
+  }
+]);
